c5b995a0b2a9956c9f8aff24b11c7cf312b6c992
freddie2074-dotcom <freddie2074@gmail.com>
2026-07-21 13:53:31 -0700

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,3 +1,4 @@
+// Navbar.jsx
 import { Link } from "react-router-dom";
 import logo from "../logo - Copy.jpeg";
 
@@ -7,23 +8,11 @@ export default function Navbar() {
       <div className="container navbar-landing-inner">
         <Link to="/" className="logo">
           <img src={logo} alt="PixellTrade" className="navbar-logo-img" />
-          PixellTrade
+          <span className="navbar-logo-text">PixellTrade</span>
         </Link>
         <nav className="navbar-landing-actions">
-          <Link to="/login" aria-label="Sign in" className="navbar-signin-icon">
-            <svg
-              width="20"
-              height="20"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            >
-              <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2" />
-              <circle cx="12" cy="7" r="4" />
-            </svg>
+          <Link to="/login" className="navbar-signin-link">
+            Sign in
           </Link>
           <Link to="/signup" className="btn btn-primary navbar-get-started">
             Get started
